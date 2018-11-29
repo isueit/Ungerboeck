78cdb831ece2fb52b59bb53e998fef87dc1b98a1
Brian Webster <bwebster@iastate.edu>
2018-11-29 15:05:01 -0600

    Gave the block config form some default values

--- ungerboeck_servsafe/src/Plugin/Block/ServSafeEvents.php
+++ ungerboeck_servsafe/src/Plugin/Block/ServSafeEvents.php
@@ -49,7 +49,7 @@ class ServSafeEvents extends BlockBase {
       '#title' => t('OrgCode'),
       '#description' => t('For ISU, this is usually 10'),
       '#size' => 15,
-      '#default_value' => isset($config['orgcode']) ? $config['orgcode'] : '10',
+      '#default_value' => $config['orgcode'],
     );
 
 //    $form['ungerboeck_fieldset']['search_string'] = array(
@@ -59,7 +59,7 @@ class ServSafeEvents extends BlockBase {
       '#description' => t('This is the OData search string.<br/>Note: #TodayDate# will be replaced by the actual date'),
       '#size' => 175,
       '#maxlength' => 300,
-      '#default_value' => isset($config['search_string']) ? $config['search_string'] : 'search_string',
+      '#default_value' => $config['search_string'],
     );
 
     return $form;
@@ -69,8 +69,20 @@ class ServSafeEvents extends BlockBase {
    * {@inheritdoc}
    */
   public function blockSubmit($form, FormStateInterface $form_state) {
-    $this->configuration['ungerboeck_servsafe_settings'] = $form_state->getValue('ungerboeck_servsafe_settings');
-    $this->configuration['orgcode'] = $form_state->getValue('orgcode');
-    $this->configuration['search_string'] = $form_state->getValue('search_string');
+    $values = $form_state->getValues();
+
+    //$this->configuration['ungerboeck_servsafe_settings'] = $form_state->getValue('ungerboeck_servsafe_settings');
+    $this->configuration['orgcode'] = $values['orgcode'];
+    $this->configuration['search_string'] = $values['search_string'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return array(
+      'orgcode' => '10',
+      'search_string' => 'Account eq \'00000150\' and Status eq \'30\' and StartDate ge DateTime\'#TodayDate#\'$orderby=StartDate,StartTime$page_size=4',
+    );
   }
 }
