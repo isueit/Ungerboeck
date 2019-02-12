cf5df22fd9d3b6633a1f8725b9d29066aca69d05
Brian Webster <bwebster@iastate.edu>
2019-02-12 10:39:17 -0600

    Started getting info on the event_details page

--- ungerboeck_eventlist/src/Controller/EventDetailsController.php
+++ ungerboeck_eventlist/src/Controller/EventDetailsController.php
@@ -15,11 +15,42 @@ class EventDetailsController extends ControllerBase {
    *   A simple renderable array.
    */
   public function event_details() {
-    $output = 'Hello World<br />';
+    $results = '';
+    $title = 'Sorry, event not found';
 
-    $output .= \Drupal::request()->query->get('eventID');
+    $eventID = intval(\Drupal::request()->query->get('eventID'));
+    $account_number  = \Drupal::request()->query->get('acct');
+    $module_config = \Drupal::config('ungerboeck_eventlist.settings');
+
+    $account_number = $config['account_number'];
+    $search_url = $module_config->get('url') . '/02-01-2019/null/null/' . $account_number;
+
+    // Fetch the page
+    $curl_handle = curl_init();
+    curl_setopt($curl_handle, CURLOPT_URL, $search_url);
+    curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 1);
+    //curl_setopt($curl_handle, CURLOPT_HEADER, 1);
+
+    /*$buffer = "<?xml version='1.0' encoding='UTF-8'?>";*/
+
+    $buffer .= curl_exec($curl_handle);
+    curl_close($curl_handle);
+
+    $json_events = json_decode(strip_tags($buffer), TRUE);
+    $json_events = array_reverse($json_events);
+
+    foreach ($json_events as $event) {
+      if ($event['EVENTID'] == $eventID) {
+        $title = $event['EVENTDESCRIPTION'];
+        $results .= $event['ANCHORVENUE'] . '<br />';
+        break;
+      }
+    }
+
     $element = array(
-      '#markup' => $output,
+      '#title' => $title,
+      '#markup' => $results,
     );
     return $element;
   }
