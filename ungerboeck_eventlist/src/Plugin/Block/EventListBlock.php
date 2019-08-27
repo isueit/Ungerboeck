bf1a9e0ad72062a7ab3959a4d6fdc683616ef375
Brian Webster <bwebster@iastate.edu>
2019-08-27 12:21:49 -0500

    Fixed an issue with search by title

--- ungerboeck_eventlist/src/Plugin/Block/EventListBlock.php
+++ ungerboeck_eventlist/src/Plugin/Block/EventListBlock.php
@@ -48,8 +48,7 @@ class EventListBlock extends BlockBase {
     }
 
     $node = \Drupal::routeMatch()->getParameter('node');
-    $string_of_search_terms = $this->build_search_string(!empty($node->field_ungerboeck_search_term->value) ?: '', $config['title_search']);
-
+    $string_of_search_terms = $this->build_search_string(!empty($node->field_ungerboeck_search_term->value) ? $node->field_ungerboeck_search_term->value : '', $config['title_search']);
     if (empty($string_of_search_terms)) {
       $show_all_events = TRUE;
       $search_terms = array ();
