26e31917217ba6e4fa40ce53a4de7a2bf1a67888
Brian Webster <bwebster@iastate.edu>
2020-04-03 10:57:02 -0500

    Handle event address, and remove extra_html (debugging)

--- ungerboeck_eventlist/src/Controller/EventDetailsController.php
+++ ungerboeck_eventlist/src/Controller/EventDetailsController.php
@@ -31,53 +31,34 @@ class EventDetailsController extends ControllerBase {
     foreach ($events_from_ungerboeck as $event) {
       if ($event['EVENTID'] == $eventID) {
 
-// ** Delete all lines with $extra_html before going live. This output is just for testing, not production **
-//$extra_html = '<hr />';
-//$extra_html .= '<strong>Raw Data:</strong><br />';
-
         $title = $event['EVENTDESCRIPTION'];
         $results .= $this->handle_dates($event) . PHP_EOL;
 
         if (empty($event['QUALTRICSID'])) {
           $results .= '  <div class="event_location">' . $event['ANCHORVENUE'] . '</div>' . PHP_EOL;
-// ** Delete all lines with $extra_html before going live. This output is just for testing, not production **
-//$extra_html .= $event['EVENTTYPECODE'] . '<br />';
-//$extra_html .= 'No Qualtrics ID given<br />';
         } else {
 
           $buffer = Helpers::hs_read_qualtrics_file();
           $events_from_qualtrics = json_decode($buffer, TRUE);
           $events_from_qualtrics['responses'] = array_reverse($events_from_qualtrics['responses']);
-          foreach ($events_from_qualtrics['responses'] as $response) {
-            if ($response['values']['_recordId'] == $event['QUALTRICSID']) {
+          foreach ($events_from_qualtrics['responses'] as $survey_response) {
+            if ($survey_response['values']['_recordId'] == $event['QUALTRICSID']) {
 
               // Found the right Qualtrics Response;
+$results .= $this->get_event_address($survey_response);
+
               if ($event['EVENTTYPECODE'] == 'NONPRIORITY') {
-                $results .= $this->handle_nonpriority_events($response);
+//                $results .= $this->handle_nonpriority_events($survey_response);
               }
               else {
-                $results .= $this->handle_priority_events($event, $response);
+//                $results .= $this->handle_priority_events($event, $survey_response);
               }
-
-// ** Delete all lines with $extra_html before going live. This output is just for testing, not production **
-//$extra_html .= str_replace('%0A', ' ', str_replace('%28', '(', str_replace('%29', ')', str_replace('%2C', ',', str_replace('+', ' ', str_replace('%40', '@', str_replace('%2F', '/', str_replace('%3A', ':', str_replace('=', ' => ', str_replace('&', '<br />', http_build_query($response['values']))))))))))) . '<br />';
-//$extra_html .= '<br />';
-//$extra_html .= str_replace('%0A', ' ', str_replace('%28', '(', str_replace('%29', ')', str_replace('%2C', ',', str_replace('+', ' ', str_replace('%40', '@', str_replace('%2F', '/', str_replace('%3A', ':', str_replace('=', ' => ', str_replace('&', '<br />', http_build_query($response['labels']))))))))))) . '<br />';
-//$extra_html .= '<br />';
-//$extra_html .= str_replace('%0A', ' ', str_replace('%28', '(', str_replace('%29', ')', str_replace('%2C', ',', str_replace('+', ' ', str_replace('%40', '@', str_replace('%2F', '/', str_replace('%3A', ':', str_replace('=', ' => ', str_replace('&', '<br />', http_build_query($response['displayedFields']))))))))))) . '<br />';
-//$extra_html .= '<br />';
-//$extra_html .= str_replace('%0A', ' ', str_replace('%28', '(', str_replace('%29', ')', str_replace('%2C', ',', str_replace('+', ' ', str_replace('%40', '@', str_replace('%2F', '/', str_replace('%3A', ':', str_replace('=', ' => ', str_replace('&', '<br />', http_build_query($response['displayedValues']))))))))))) . '<br />';
 
               break;
             }
           }
         }
         $results .= $this->get_registration_info($event) . PHP_EOL;
-
-// ** Delete all lines with $extra_html before going live. This output is just for testing, not production **
-//$extra_html .= '<br />';
-//$extra_html .= str_replace('%5B', '[', str_replace('%5D', ']', str_replace('%0A', ' ', str_replace('%28', '(', str_replace('%29', ')', str_replace('%2C', ',', str_replace('+', ' ', str_replace('%40', '@', str_replace('%2F', '/', str_replace('%3A', ':', str_replace('=', ' => ', str_replace('&', '<br />', http_build_query($event))))))))))))) . '<br />';
-//$results .= $extra_html;
 
         // We've found the correct event, quit looking for the right event
         break;
@@ -110,7 +91,7 @@ class EventDetailsController extends ControllerBase {
     }
 
     $output = '  <div class="event_details_registration">' . $output . '</div>';
-  
+
     return $output;
   }
 
@@ -156,15 +137,16 @@ class EventDetailsController extends ControllerBase {
     $results = '';
     $mydescription = '  <div class="event_description">' . str_replace(' target="_blank"', '', $list_of_descriptions[$response['values']['QID1']]) . '</div>';
 
-    if (!empty($response['values']['QID8_1'])) {
-      $results .= '  <div class="event_address">' . $response['values']['QID8_1'] . '<br />' . PHP_EOL;
-      if (!empty($response['values']['QID8_2'])) { $results .= '    ' . $response['values']['QID8_2'] . '<br />' . PHP_EOL; }
-      if (!empty($response['values']['QID8_3'])) { $results .= '    ' . $response['values']['QID8_3'] . ', '; }
-      if (!empty($response['values']['QID8_4'])) { $results .= $response['values']['QID8_4'] . ' '; }
-      if (!empty($response['values']['QID8_5'])) { $results .= $response['values']['QID8_5']; }
-      $results .= '  </div>' . PHP_EOL;
-      $results .= $mydescription . PHP_EOL;
-    }
+    //if (!empty($response['values']['QID8_1'])) {
+      //$results .= '  <div class="event_address">' . $response['values']['QID8_1'] . '<br />' . PHP_EOL;
+      //if (!empty($response['values']['QID8_2'])) { $results .= '    ' . $response['values']['QID8_2'] . '<br />' . PHP_EOL; }
+      //if (!empty($response['values']['QID8_3'])) { $results .= '    ' . $response['values']['QID8_3'] . ', '; }
+      //if (!empty($response['values']['QID8_4'])) { $results .= $response['values']['QID8_4'] . ' '; }
+      //if (!empty($response['values']['QID8_5'])) { $results .= $response['values']['QID8_5'] . '<br />' . PHP_EOL; }
+      //if (!empty($response['values']['QID8_6'])) { $results .= $response['values']['QID8_6'] . '<br />' . PHP_EOL; }
+      //$results .= '  </div>' . PHP_EOL;
+      //$results .= $mydescription . PHP_EOL;
+    //}
 
     if (!empty($response['values']['QID105_5'])) {
       $results .= '  <div class="event_location">' . $response['values']['QID105_5'] . '</div>' . PHP_EOL;
@@ -200,6 +182,29 @@ class EventDetailsController extends ControllerBase {
     }
 
     return $results;
+  }
+
+  private function get_event_address($survey_response) {
+    $event_address = '';
+
+    if (!empty($survey_response['values']['QID8_1'])) {
+      if (substr($survey_response['values']['QID8_1'], 0, 7) === 'http://' || substr($survey_response['values']['QID8_1'], 0, 8) === 'https://') {
+        $event_address .= '<a href="' . trim($survey_response['values']['QID8_1']) . '">' . trim($survey_response['values']['QID8_1']) . '</a><br />' . PHP_EOL;
+      } else {
+        $event_address .= $survey_response['values']['QID8_1'] . '<br />' . PHP_EOL;
+      }
+
+      if (!empty($survey_response['values']['QID8_2'])) { $event_address .= '    ' . $survey_response['values']['QID8_2'] . '<br />' . PHP_EOL; }
+      if (!empty($survey_response['values']['QID8_3'])) { $event_address .= '    ' . $survey_response['values']['QID8_3'] . ', '; }
+      if (!empty($survey_response['values']['QID8_4'])) { $event_address .= $survey_response['values']['QID8_4'] . ' '; }
+      if (!empty($survey_response['values']['QID8_5'])) { $event_address .= $survey_response['values']['QID8_5'] . '<br />' . PHP_EOL; }
+      if (!empty($survey_response['values']['QID8_6'])) { $event_address .= str_replace("\n", '<br />', $survey_response['values']['QID8_6']) . PHP_EOL; }
+    }
+
+    if (!empty($event_address)) {
+      $event_address = '  <div class="event_address">' . $event_address . '  </div>' . PHP_EOL;
+    }
+    return($event_address);
   }
 
 }
